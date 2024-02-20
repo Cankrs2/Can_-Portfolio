7ec2d8611f2f4c75cdee1bd8fa75bc0c1cffa2a4
Cankrs2 <94618678+Cankrs2@users.noreply.github.com>
2024-02-20 19:41:40 +0000

    Pending changes exported from your codespace

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -62,11 +62,7 @@ const Portfolio = () => {
       <h2 style={{ textAlign: "center" }}>Portfolio</h2>
       <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
         <div style={{ maxWidth: "40%", alignSelf: "center" }}>
-          <img
-            src={image}
-            style={{ height: "90%", width: "100%", objectFit: "cover" }}
-            alt={imageAltText}
-          />
+        <img src={image} style={{ height: "90%", width: "100%", objectFit: "cover", animation: "1s ease-out 0s 1 slideIn" }} />
         </div>
         <div className="container">
           {projectList.map((project) => (
